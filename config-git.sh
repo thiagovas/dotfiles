2400a7708118d10e1bb45d55ee9d6a2f876e23f2
Thiago Silva <thiagovieiraas@gmail.com>
2017-12-03 15:46:04 -0200

    Updating email and user in config-git as well.

--- config-git.sh
+++ config-git.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # Setting git's configs...
 
+
+git config --global user.name "Thiago Silva"
+
+current_email=$(git config --global --get user.email)
+
+#if current_email is empty, then add an email
+if [[ -z "$current_email" ]]
+then
+  git config --global user.email "thiagovieiraas@gmail.com"
+fi
+
+git config --global github.user thiagovas
+
+
 # https://help.github.com/articles/caching-your-github-password-in-git/
 git config --global credential.helper cache
 
