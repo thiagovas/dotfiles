a5f2206879d24762a5f9dbc639e84a0abf8b6f06
Thiago Silva <thiagovieiraas@gmail.com>
2018-02-16 17:07:37 -0200

    Added a setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install autotools-dev automake
+
+
+# Installing ncurses
+sudo apt-get install libncurses5 libncurses5-dev
+
+# Installing tmux
+sudo apt-get install tmux
