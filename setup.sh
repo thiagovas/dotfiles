25e9dd891c437974c865383a4e99d5c522c4b2ae
Thiago Silva <thiagovieiraas@gmail.com>
2019-02-09 18:03:20 -0200

    Making sure curl is installed as well.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install autotools-dev automake build-essential cmake
+sudo apt-get install autotools-dev automake build-essential cmake curl
 
 
 # Installing ncurses
